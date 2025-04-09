fb998c7d3958753fdedfea9c20b3e9abf93f3c80
fefeisabely <159018859+fefeisabely@users.noreply.github.com>
2025-04-09 12:51:40 +0000

    vercel alterações

--- src/App.jsx
+++ src/App.jsx
@@ -8,27 +8,7 @@ const App = ()=> {
     <>
       <h3>Mamae te amo</h3>
       <Etiqueta texto="voce é muito linda" />
-      <Etiqueta texto="mamaMãe, sou grata por tudo que fez por mim
-Cada gesto de carinho que recebi
-Nunca vou esquecer, como eu amo você
-Tantas alegrias nós iremos viver
-
-Mamãe, que Deus abençoe sua vida
-És a rosa mais linda do meu jardim
-Mamãe, que Deus abençoe sua vida
-És a rosa mais linda do meu jardim
-
-Mãe, sou grata por tudo que fez por mim
-Cada gesto de carinho que recebi
-Nunca vou esquecer, como eu amo você
-Tantas alegrias nós iremos viver
-
-Mamãe, que Deus abençoe sua vida
-És a rosa mais linda do meu jardim
-Mamãe, que Deus abençoe sua vida
-És a rosa mais linda do meu jardim
-
-Mamãe, que Deus abençoe sua vida
+      <Etiqueta texto="Mamãe, que Deus abençoe sua vida
 És a rosa mais linda do meu jardim" />
       <Etiqueta />
       <Caixa />
