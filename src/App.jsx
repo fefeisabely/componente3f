db9e0afadffbbcc01c140b69eeb067ed1c9077aa
fefeisabely <159018859+fefeisabely@users.noreply.github.com>
2025-04-09 12:55:46 +0000

    vercel alterações

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ const App = ()=> {
     <>
       <h3>Mamae te amo</h3>
       <Etiqueta texto="voce é muito linda" />
-      <Etiqueta texto="Mamãe, que Deus abençoe sua vida
-És a rosa mais linda do meu jardim" />
+      <Etiqueta texto="Mamãe, que Deus abençoe sua vida! 
+      És a rosa mais linda do meu jardim" />
       <Etiqueta />
       <Caixa />
       <Botao />
